aeb7e37f57e6542523e62a20f692dbc724e3950a
Giovanna Diniz <giovanna.diniz.56@gmail.com>
2026-01-29 01:07:32 -0300

    feat: job melhorado

--- src/main/kotlin/com/bolt/desafio/Job/CsvAneelJob.kt
+++ src/main/kotlin/com/bolt/desafio/Job/CsvAneelJob.kt
@@ -5,56 +5,68 @@ import com.bolt.desafio.Repository.UsinaRepository
 import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
-import java.net.URL
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.URI
 
 @Component
 class CsvAneelJob (
     private val repository: UsinaRepository
 ) {
     private val logger = LoggerFactory.getLogger(javaClass)
-    private val csvUrl = "https://dadosabertos.aneel.gov.br/dataset/ralie-relatorio-de-acompanhamento-da-expansao-da-oferta-de-geracao-de-energia-eletrica/resource/a3c58ecb-e936-4dc1-884b-9941f7079a73/download/ralie-usina.csv"
 
-    @Scheduled(cron = "0 */10 * * * *")
+    // Use URI para evitar o 'deprecated'
+    private val url =
+        URI.create("https://dadosabertos.aneel.gov.br/dataset/57e4b8b5-a5db-40e6-9901-27ca629d0477/resource/4a615df8-4c25-48fa-bbea-873a36a79518/download/ralie-usina.csv")
+            .toURL()
+
+    @Scheduled(initialDelay = 5000, fixedDelay = 600000)
     fun processarCargaUsinas() {
-        logger.info("Iniciando Job de extração da ANEEL...")
+        logger.info("Iniciando processamento otimizado...")
 
-        try {
-            val connection = URL(csvUrl).openConnection()
-            connection.setRequestProperty("User-Agent", "Mozilla/5.0")
+        val stream = url.openStream()
+        val reader = BufferedReader(InputStreamReader(stream, Charsets.ISO_8859_1))
 
-            val usinas = URL(csvUrl).openConnection().getInputStream()
-                .bufferedReader(Charsets.ISO_8859_1).useLines { lines ->
-                    lines.drop(1)
-                        .filter { it.isNotBlank() }
-                        .map { linha ->
-                            val colunas = linha.split(";")
-                            Usina(
-                                ceg = colunas[3].limpar(),
-                                nome = colunas[8].limpar(),
-                                agente = colunas[1].limpar(),
-                                combustivel = colunas[7].limpar(),
-                                uf = colunas[5].limpar(),
-                                potenciaKw = formatarPotencia(colunas[10])
-                            )
-                        }.toList()
+        // 1. Descobrir os índices (Igual ao outro candidato)
+        val header = reader.readLine()?.split(";")?.map { it.trim('"').trim() } ?: return
+        logger.info("Cabeçalho detectado: $header")
+        val idx = mapOf(
+            "ceg" to header.indexOf("CodCEG"),
+            "nome" to header.indexOf("NomEmpreendimento"),
+            "uf" to header.indexOf("SigUFPrincipal"),
+            "comb" to header.indexOf("DscOrigemCombustivel"),
+            "pot" to header.indexOf("MdaPotenciaOutorgadaKw"),
+            "agente" to header.indexOf("NomEmpresaConexao")
+        )
+
+        // Limpamos o banco antes de começar (Requisito 2)
+        repository.deleteAllInBatch()
+
+        // 2. O Pulo do Gato: Processar em CHUNKS (Lotes)
+        reader.lineSequence()
+            .mapNotNull { linha ->
+                val colunas = linha.split(";")
+                try {
+                    Usina(
+                        ceg = colunas[idx["ceg"]!!].trim('"'),
+                        nome = colunas[idx["nome"]!!].trim('"'),
+                        agente = colunas[idx["agente"]!!].trim('"'),
+                        combustivel = colunas[idx["comb"]!!].trim('"'),
+                        uf = colunas[idx["uf"]!!].trim('"'),
+                        potenciaKw = colunas[idx["pot"]!!].trim('"')
+                            .replace(".", "").replace(",", ".").toDoubleOrNull() ?: 0.0
+                    )
+                } catch (e: Exception) {
+                    logger.error("Lote de usinas com erro: "+ e )
+                    null
                 }
+            }
+            .chunked(1000) // Agrupa de 1000 em 1000
+            .forEach { lote ->
+                repository.saveAll(lote)
+                logger.info("Lote de ${lote.size} usinas salvo...")
+            }
 
-            logger.info("Limpando banco e salvando ${usinas.size} registros...")
-            repository.deleteAllInBatch()
-            repository.saveAll(usinas)
-
-            logger.info("Carga finalizada com sucesso! ${usinas.size} usinas importadas.")
-
-        } catch (e: Exception) {
-            logger.error("Falha ao processar o Job: ${e.message}")
-        }
+        logger.info("Carga concluída com sucesso!")
     }
-
-    private fun formatarPotencia(valor: String): Double {
-        return valor.replace(".", "")
-            .replace(",", ".")
-            .toDoubleOrNull() ?: 0.0
-    }
-
-    private fun String.limpar() = this.removeSurrounding("\"").trim()
 }
