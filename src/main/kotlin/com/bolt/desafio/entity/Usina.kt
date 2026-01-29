85e2523cd70331fc4de5835b3f8e9b9048feed6f
Giovanna Diniz <giovanna.diniz.56@gmail.com>
2026-01-29 00:01:12 -0300

    feat: entidade Usina implementada

---
+++ src/main/kotlin/com/bolt/desafio/entity/Usina.kt
@@ -0,0 +1,31 @@
+package com.bolt.desafio.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "usinas")
+data class Usina(
+    @Id
+    @Column(name = "ceg")
+    val ceg: String,
+
+    @Column(name = "nome_usina")
+    val nome: String,
+
+    @Column(name = "agente")
+    val agente: String,
+
+    @Column(name = "combustivel")
+    val combustivel: String,
+
+    @Column(name = "estado_uf")
+    val uf: String,
+
+    @Column(name = "potencia_kw")
+    val potenciaKw: Double
+) {
+    constructor() : this("", "", "", "", "", 0.0)
+}
