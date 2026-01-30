c6861da5033601f80d6aeff1b24468dd326ef4fe
Giovanna Diniz <giovanna.diniz.56@gmail.com>
2026-01-29 23:42:30 -0300

    feat: removendo comentrários e renomeando arquivo

--- src/main/resources/db/migration/V2026.01.28.21.18__create_table_usina.sql
+++ src/main/resources/db/migration/V2026.01.28.21.18__create_table_usina.sql
@@ -1,6 +1,5 @@
 DO $$
 BEGIN
-    -- 1. Criar a tabela apenas se ela não existir
     IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'usinas') THEN
 CREATE TABLE energia.usina (
                         ceg VARCHAR(50) PRIMARY KEY,
@@ -12,7 +11,6 @@ CREATE TABLE energia.usina (
 );
 END IF;
 
-    -- 2. Criar o índice apenas se ele não existir
     IF NOT EXISTS (SELECT 1 FROM pg_indexes WHERE indexname = 'idx_potencia') THEN
 CREATE INDEX idx_potencia ON energia.usina (potencia_kw DESC);
 END IF;
