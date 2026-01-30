74b55df121cbdb7072ca011337ce01515b01e4b7
Giovanna Diniz <giovanna.diniz.56@gmail.com>
2026-01-30 10:13:25 -0300

    feat: config de cors implementada

--- src/main/kotlin/com/bolt/desafio/Controller/UsinaController.kt
+++ src/main/kotlin/com/bolt/desafio/Controller/UsinaController.kt
@@ -2,14 +2,12 @@ package com.bolt.desafio.Controller
 
 import com.bolt.desafio.Service.UsinaService
 import com.bolt.desafio.dto.UsinaDTO
-import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping( "/api/usinas")
-@CrossOrigin(origins = ["http://localhost:4200"])
 class UsinaController (private val service: UsinaService){
 
     @GetMapping("/maiores")
