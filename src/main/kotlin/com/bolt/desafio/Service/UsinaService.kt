98d99e2e4b97018c30018e008ad429eb9f66493d
Giovanna Diniz <giovanna.diniz.56@gmail.com>
2026-01-29 23:43:54 -0300

    feat: camada service criada para melhor organizaçao.

--- src/main/kotlin/com/bolt/desafio/Service/UsinaService.kt
+++ src/main/kotlin/com/bolt/desafio/Service/UsinaService.kt
@@ -1,7 +1,24 @@
 package com.bolt.desafio.Service
 
+import com.bolt.desafio.Repository.UsinaRepository
+import com.bolt.desafio.dto.UsinaDTO
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
-class UsinaService {
+class UsinaService (private val repository: UsinaRepository) {
+
+    companion object {
+        private val log = LoggerFactory.getLogger(UsinaService::class.java)
+    }
+
+    fun buscarTop5Maiores(): List<UsinaDTO> {
+        return try {
+            repository.findTop5ByOrderByPotenciaKwDesc()
+        } catch (e: Exception) {
+            log.error("Erro ao buscar usinas: ${e.message}")
+            emptyList()
+        }
+    }
+
 }
