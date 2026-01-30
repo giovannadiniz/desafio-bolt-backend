62ea01407a92fb7dfe02290605334132ad8bcb5d
Giovanna Diniz <giovanna.diniz.56@gmail.com>
2026-01-29 23:44:54 -0300

    feat: mudança para utilizar dto e annotation para crossOrigin

--- src/main/kotlin/com/bolt/desafio/Controller/UsinaController.kt
+++ src/main/kotlin/com/bolt/desafio/Controller/UsinaController.kt
@@ -1,15 +1,17 @@
 package com.bolt.desafio.Controller
 
-import com.bolt.desafio.Entity.Usina
-import com.bolt.desafio.Repository.UsinaRepository
+import com.bolt.desafio.Service.UsinaService
+import com.bolt.desafio.dto.UsinaDTO
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping( "/api/usinas")
-class UsinaController (private val repository: UsinaRepository){
+@CrossOrigin(origins = ["http://localhost:4200"])
+class UsinaController (private val service: UsinaService){
 
     @GetMapping("/maiores")
-    fun listarMaiores(): List<Usina> = repository.findTop5ByOrderByPotenciaKwDesc()
+    fun listarMaiores(): List<UsinaDTO> = service.buscarTop5Maiores()
 }
--- src/main/kotlin/com/bolt/desafio/Repository/UsinaRepository.kt
+++ src/main/kotlin/com/bolt/desafio/Repository/UsinaRepository.kt
@@ -1,11 +1,12 @@
 package com.bolt.desafio.Repository
 
 import com.bolt.desafio.Entity.Usina
+import com.bolt.desafio.dto.UsinaDTO
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface UsinaRepository : JpaRepository<Usina, String> {
 
-    fun findTop5ByOrderByPotenciaKwDesc(): List<Usina>
+    fun findTop5ByOrderByPotenciaKwDesc(): List<UsinaDTO>
 }
