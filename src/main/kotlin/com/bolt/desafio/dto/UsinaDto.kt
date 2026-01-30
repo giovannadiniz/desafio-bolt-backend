740b02ce5e038ed3676a3055e97bc95510c715db
Giovanna Diniz <giovanna.diniz.56@gmail.com>
2026-01-29 23:43:21 -0300

    feat: dto criado, serve tanto para response quanto request

---
+++ src/main/kotlin/com/bolt/desafio/dto/UsinaDto.kt
@@ -0,0 +1,10 @@
+package com.bolt.desafio.dto
+
+data class UsinaDTO(
+    val ceg: String,
+    val nome: String,
+    val agente: String,
+    val combustivel: String,
+    val uf: String,
+    val potenciaKw: Double
+)
